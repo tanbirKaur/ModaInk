6857f682a0e60fde9bbb0436939629fb2d176014
Taj Ahmed <go4taj@gmail.com>
2016-12-18 13:29:09 +0530

    Add service to get products to specific designer

--- app/js/controllers/designersController.js
+++ app/js/controllers/designersController.js
@@ -7,6 +7,10 @@ app.controller('DesignersController', function($scope,$stateParams, httpService)
 	$scope.getDesigners = function () {
 		httpService.callHttp("GET","designers",{},{},{},$scope.onGetDesignersSuccess,$scope.onGetDesignersFailure,true);
 	}
+	$scope.getDesignerProducts = function (designerId) {
+		var params = {designerId : designerId};
+		httpService.callHttp("GET","designers",params,{},{},$scope.onGetDesignersSuccess,$scope.onGetDesignersFailure,true);
+	}
 
 	// http Success and Failure Methods
 	$scope.onGetDesignerDetailsSuccess = function (response) {
@@ -33,9 +37,26 @@ app.controller('DesignersController', function($scope,$stateParams, httpService)
 	$scope.onGetDesignersFailure = function (response) {
 		console.log("onGetDesignersFailure",response);
 	}
+
+	$scope.onGetDesignerProductsSuccess = function (response) {
+		var designersFound = response.statusText == "OK";
+		if (designersFound) {
+			var designers = response.data;
+			// map designers to alphabets
+			$scope.designerList = designers.map(function(designer){
+				designer.alphabet = designer.firstName[0];
+				return designer;
+			});
+		};
+	}
+	$scope.onGetDesignerProductsFailure = function (response) {
+		console.log("onGetDesignerProductsFailure",response);
+	}
+
 	var designerId = $stateParams.designerId;
 	if (designerId) {
 		$scope.getDesignerDetails(designerId);
+		$scope.getDesignerProducts(designerId);
 	} else {
 		$scope.getDesigners();
 	}
