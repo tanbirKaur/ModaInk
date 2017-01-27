2c525ffada678a98a6e81f93c4efe21817ef871a
TajAhmed <go4taj@gmail.com>
2017-01-27 20:48:37 +0530

    add visual effect on product sku selection

--- app/app.js
+++ app/app.js
@@ -137,7 +137,19 @@ window.apiUrl = "http://modaink.com/api";
     	localStorageServiceProvider.setPrefix('modaink')
     	.setStorageType('sessionStorage');
 });
-
+app.directive('script', function() {
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function(scope, elem, attr) {
+            if (attr.type=='text/javascript-lazy') {
+                var code = elem.text();
+                var f = new Function(code);
+                f();
+            }
+        }
+    };
+});
 //Global helper methods
 Array.prototype.contains = function(obj) {
     var i = this.length;
