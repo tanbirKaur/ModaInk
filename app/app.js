1e45ae4f85011c7b7cc991df3af9619c88b990de
TajAhmed <go4taj@gmail.com>
2017-02-01 08:07:57 +0530

    Update app.js to change array equal to work filters properly

--- app/app.js
+++ app/app.js
@@ -163,17 +163,12 @@ Array.prototype.contains = function(obj) {
 
 window.isEquivalent = function (a, b) {
 	
-    // Create arrays of property names
+    // Create array of property name
     var aProps = Object.getOwnPropertyNames(a);
-    var bProps = Object.getOwnPropertyNames(b);
-
-    // If number of properties is different, objects are not equivalent
-    if (aProps.length != bProps.length) {
-        return false;
-    }
 
     for (var i = 0; i < aProps.length; i++) {
         var propName = aProps[i];
+        if(propName.startsWith("$")) continue;
         // If values of same property are not equal, objects are not equivalent
         if (a[propName] !== b[propName]) {
            return false;
